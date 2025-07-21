c5e396d7401f124e027e5128b4bf2aec33ee0b4e
Nayk08 <kyanvillarin60@gmail.com>
2025-07-21 16:27:52 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tic-tac-toe/',
+  base: process.env.VITE_BASE_PATH || "/Nayk08/Tic-Tac-Toe",
 })
